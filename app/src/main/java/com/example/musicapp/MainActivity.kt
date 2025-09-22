4dd936093672605911d0cbe15dc20bf7e13bd323
mclaverof24 <mclaverof24@fpsanturtzilh.eus>
2025-09-22 17:02:18 +0200

    Lehenengo comitta, xml bertikalki egiten

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.musicapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var isPlaying = false
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Ajuste de insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // --- PLAY/PAUSE ---
+        val playPauseButton: ImageButton = findViewById(R.id.playPauseButton)
+
+        playPauseButton.setOnClickListener {
+            isPlaying = !isPlaying
+            if (isPlaying) {
+                playPauseButton.setImageResource(R.drawable.ic_pause) // icono de pausa
+            } else {
+                playPauseButton.setImageResource(R.drawable.ic_play) // icono de play
+            }
         }
     }
 }
